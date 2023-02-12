e0a91b88de6709bd269361e94a4d3a51a1c26905
Haf <haf@protonmail.ch>
2023-02-12 20:09:05 +0100

    a note

--- sample-create-maven-repo.sh
+++ sample-create-maven-repo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 http -a admin:350e0bcb-657a-4d54-8c36-82a20747fa68  \
-  POST http://10.233.1.2:8081/service/rest/v1/repositories/maven/hosted/ \
+  POST http://nixus:8081/service/rest/v1/repositories/maven/hosted/ \
   "name=example" \
   "online:=true" \
   "storage[blobStoreName]=default" \
