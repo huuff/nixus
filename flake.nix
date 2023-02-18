0fe8ae741258d0c5c611218e63fcf90c31538473
Haf <haf@protonmail.ch>
2023-02-18 17:03:19 +0100

    nixos 22.11 seems to work OK

--- flake.nix
+++ flake.nix
@@ -2,10 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    #nixpkgs.url = "nixpkgs/nixos-22.05";
-    # XXX: Only unstable currently has nexus > 3.4.0
-    # which includes a privilege for healthchecks I need for my tests
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-22.11";
   };
 
   outputs = { self, nixpkgs }:
